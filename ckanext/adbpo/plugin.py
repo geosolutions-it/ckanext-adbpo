a2992e3012b4d369b70cce79b1e2f7b6ea73bcfe
etj <etj@geo-solutions.it>
2022-01-21 18:41:52 +0100

    Fix index page

--- ckanext/adbpo/plugin.py
+++ ckanext/adbpo/plugin.py
@@ -3,7 +3,9 @@ import ckan.plugins.toolkit as toolkit
 
 
 class ADBPoUiPlugin(plugins.SingletonPlugin):
+
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +13,20 @@ class ADBPoUiPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('assets', 'adbpo')
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {
+            'adbpo_get_groups': adbpo_get_groups,
+        }
+
+
+def adbpo_get_groups(limit=10):
+    return toolkit.get_action('group_list')\
+        (
+            data_dict={
+                'sort': 'package_count desc',
+                'all_fields': True,
+                'limit': limit
+            }
+        )
